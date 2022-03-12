0f4a48b5d2d57aa854b79818d89f06c3b5f0394b
Matt Wilson <mattwwilson34@gmail.com>
2022-03-11 21:10:47 -0500

    Added Button onclick test

--- src/Components/Button/__test__/Button.test.js
+++ src/Components/Button/__test__/Button.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Button from '../Button';
 
 describe('Button component', () => {
   it('renders correctly with correct Label', () => {
     render(<Button label='RESET' />);
     expect(screen.getByRole('button').textContent).toMatch('RESET');
+  });
+
+  it('Calls reset function after button click', () => {
+    const reset = jest.fn(() => '');
+    render(<Button label='RESET' onClick={reset} />);
+    const button = screen.getByRole('button', { name: 'RESET' });
+
+    userEvent.click(button);
+
+    expect(reset).toHaveBeenCalled();
   });
 });
