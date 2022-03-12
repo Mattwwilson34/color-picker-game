6801b6c208939b684a43d4d15faa9e8cd79f032d
Matt Wilson <mattwwilson34@gmail.com>
2022-03-11 20:40:19 -0500

    Added Button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import RgbDisplay from './Components/RgbDisplay';
 import ColorBlock from './Components/ColorBlock';
 import randomRGB from './Utils/randomRGB';
-import React, { useState, useEffect } from 'react';
+import Button from './Components/Button';
 
 function App() {
   const [winColor, setWinColor] = useState(randomRGB());
@@ -24,9 +25,12 @@ function App() {
     <div className='App'>
       <h1>Color Picker Game</h1>
       <RgbDisplay RGB={winColor} />
-      {colorArray.map((color) => {
-        return <ColorBlock key={color} color={color} />;
-      })}
+      <div className='ColorBlockContainer'>
+        {colorArray.map((color) => {
+          return <ColorBlock key={color} color={color} />;
+        })}
+      </div>
+      <Button label='RESET' onClick={() => console.log('Clicked')} />
     </div>
   );
 }
